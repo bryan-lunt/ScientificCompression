e5365d190b6cd0c605f9fc44d4271747b724f99c
Bryan Lunt <bjlunt2@illinois.edu>
2026-08-11 16:51:38 -0700

    improved bash file

--- test.bash
+++ test.bash
@@ -1,10 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Removing old files if any."
 rm *.zip test2.txt* reconstituted.txt
 
 #curl http://www.gutenberg.org/cache/epub/1122/pg1122.txt > hamlet.txt
 
+set -e
 
+echo "Producing random input"
 python produce_random_input.py > test2.txt
+
+echo "Saparating into ASCII and binary files"
 python separate.py test2.txt
+
+echo "Zipping into a two zip files for comparison"
 zip -9 normal.zip test2.txt
 zip -9 sep.zip test2.txt_*
 
@@ -13,6 +22,8 @@ SEPARATE_SIZE=$(ls -l sep.zip | awk '{print $5;}')
 
 echo ratio of zip file sizes is $(bc <<< "scale=3; ${SEPARATE_SIZE} / ${NORMAL_SIZE} ")
 
+
+echo "Recombining files to test losslessness"
 python combine.py test2.txt_text test2.txt_numeric test2.txt_table > reconstituted.txt
 diff -q test2.txt reconstituted.txt
 
